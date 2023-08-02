40d786c759a06ec1d822ccca05110c3c8d00a3e6
Vanderley Pereira <vanderley.filho@outlook.com>
2023-08-02 19:16:38 -0300

    User edit e delete

--- gerenciamento_api/views.py
+++ gerenciamento_api/views.py
@@ -1,1 +1,34 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from autenticacao.models import User
+from .serializers import UserSerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+
+@permission_classes([IsAuthenticated])
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        response.data['message'] = 'Usuário criado com sucesso.'
+        return response
+    
+@permission_classes([IsAuthenticated])
+class UpdateDeleteUserView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    lookup_field = 'id'
+
+    def update(self, request, *args, **kwargs):
+        response = super().update(request, *args, **kwargs)
+        response.data['message'] = 'Usuário atualizado com sucesso.'
+        return response
+
+    def delete(self, request, *args, **kwargs):
+        response = super().delete(request, *args, **kwargs)
+        if response.status_code == 204:
+            return Response({'message': 'Usuário deletado com sucesso.'}, status=200)
+        return response
