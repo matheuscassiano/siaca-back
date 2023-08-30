42295e99abe0a5110825fab7c07bf342b57c94b3
Marcus Vinicius <marcusvpestana@gmail.com>
2023-08-30 15:39:55 -0300

    oferta class

--- coordenacao/models.py
+++ coordenacao/models.py
@@ -55,3 +55,27 @@ class Disciplina(models.Model):
 
     def get_absolute_url(self):
         return reverse("disciplina_detail", kwargs={"pk": self.pk})
+    
+class Oferta(models.Model):
+    # periodo = models.ForeignKey(Periodo, on_delete=models.CASCADE, null=False, blank=False)
+    disciplina = models.ForeignKey(Disciplina, on_delete=models.DO_NOTHING, null=False, blank=False)
+    professor = models.ForeignKey(swappable_dependency('autenticacao.Professor'), on_delete=models.DO_NOTHING, null=True, blank=True)
+    sala = models.ForeignKey(Sala, on_delete=models.DO_NOTHING, null=True, blank=True)
+    aula_dias = models.CharField(max_length=7, null=False, blank=False) # '{0-6}' cada numero representa um dia da semana
+    aula_hora_inicio = models.TimeField(null=False, blank=False)
+    aula_hora_fim = models.TimeField(null=False, blank=False)
+    create_date = models.DateField(auto_now_add=True)
+    update_date = models.DateField(auto_now=True)
+
+    class Meta:
+        verbose_name = _("oferta")
+        verbose_name_plural = _("ofertas")
+
+    def __str__(self):
+        if self.professor == None:
+            return f"{self.pk} | {self.disciplina.nome} | Sem professor"
+        else:
+            return f"{self.pk} | {self.disciplina.nome} | {self.professor.user.first_name}"
+
+    def get_absolute_url(self):
+        return reverse("oferta_detail", kwargs={"pk": self.pk})
